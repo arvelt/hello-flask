39f97ccc231afa4f1d2ca15248d41a906380088e
Keisuke Ota <arvelt.s@gmail.com>
2015-06-20 21:21:06 +0900

    modify script

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i hosts -u ${USER_NAME} --extra-vars '{"HOST":"production"}' site.yml
+ansible-playbook -i hosts -u ${USER_NAME} --extra-vars '{"HOST":"production", "SUDOPASS":"${SUDOPASS}"}' site.yml
