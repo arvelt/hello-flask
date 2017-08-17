d67a8684f77837150997239471819bb65afb7329
Keisuke Ota <arvelt.s@gmail.com>
2017-08-17 16:25:42 +0900

    reset strucuter

--- deploy_devlopment.sh
+++
@@ -1,1 +0,0 @@
-ansible-playbook -i hosts -u vagrant --extra-vars '{"HOST":"vagrant"}' site.yml
--- deploy_production.sh
+++
@@ -1,6 +0,0 @@
-vars="{
-    \"APP_USER\":\"$USER_NAME\",
-    \"HOST\":\"production\",
-    \"ansible_sudo_pass\":\"$SUDOPASS\"
-}"
-ansible-playbook -i hosts -u "${USER_NAME}" --extra-vars "${vars}" site.yml
