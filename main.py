6b2b7702db41009a9fcf1428b8f133299e0e0d81
Keisuke Ota <arvelt.s@gmail.com>
2017-08-17 17:34:35 +0900

    add raw response

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from flask import Flask, jsonify
+from flask import make_response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/raw_response")
+def get():
+    template = u"""
+    <!DOCTYPE html>
+    <html>
+    <body>
+        Raw string template.
+    </body>
+    </html>
+    """
+    response = make_response(template)
+    response.headers['X-Parachutes'] = 'parachutes are cool'
+    return response
 
 @app.route("/data")
 def get_data():
@@ -11,4 +26,4 @@ def get_data():
     return jsonify(res)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
